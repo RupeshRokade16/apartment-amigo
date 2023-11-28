e15ffb08f4a33b5a520cedc0ef114045cfd1cb16
RupeshRokade16 <rokade.r@northeastern.edu>
2023-11-27 23:10:18 -0500

    updated gitignore with env file

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-require("dotenv").config({ path: "./config.env" });
+require("dotenv").config({ path: "./configure.env" });
 
 // Set up the port, default to 5000 if not provided in the environment
 const port = process.env.PORT || 5000;
