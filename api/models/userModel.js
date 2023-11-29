cecb4422503509239e21eb7003b2449fe08fde3e
RupeshRokade16 <rokade.r@northeastern.edu>
2023-11-28 21:38:57 -0500

    added minlength in user schema

--- api/models/userModel.js
+++ api/models/userModel.js
@@ -5,7 +5,8 @@ const userSchema = mongoose.Schema({
     username: {
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        minlength: 3 
     },
     email: {
         type:String,
@@ -15,6 +16,7 @@ const userSchema = mongoose.Schema({
     password: {
         type:String,
         required: true,
+        minlength: 6
     },
 
     //Other Fields
