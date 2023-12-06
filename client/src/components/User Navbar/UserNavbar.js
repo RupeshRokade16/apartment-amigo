3b9cd5044ff906de0ad161a4927017187f75e9e8
RupeshRokade16 <rokade.r@northeastern.edu>
2023-12-06 10:26:27 -0500

    User navbar styles

--- client/src/components/User Navbar/UserNavbar.js
+++ client/src/components/User Navbar/UserNavbar.js
@@ -1,32 +1,37 @@
-  import React from 'react';
-  import Navbar from 'react-bootstrap/Navbar';
-  import Nav from 'react-bootstrap/Nav';
-  import { Link } from 'react-router-dom';
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { Link } from "react-router-dom";
 
-  import './UserNavbar.css';
+import "./UserNavbar.css";
 
-  const UserNavbar = (props) => {
-    const {user, handleLogout} = props;
-    const userProfilePath = '/profile';
-    // const userProfileProps = user ; // Replace with your actual prop and value
+const UserNavbar = (props) => {
+  const { user, handleLogout } = props;
+  const userProfilePath = "/profile";
+  // const userProfileProps = user ; // Replace with your actual prop and value
 
-    return (
-      <div>
-        {/* Pass custom props using the "to" prop and the "state" object */}
-        <Link to={{ pathname: userProfilePath, state: {user} }} className='site-title'>
-          Profile {user.username}
-        </Link>
-  
-        <ul>
-          <li>
-            {/* You can also pass props in a similar way for other Links */}
-            <Link to="#" onClick={handleLogout}>
-              Logout
-            </Link>
-          </li>
-        </ul>
-      </div>
-    );
-  };
+  return (
+    <div className="userNav">
+      {/* Pass custom props using the "to" prop and the "state" object */}
 
-  export default UserNavbar;
+      <ul>
+        <li>
+          <Link
+            to={{ pathname: userProfilePath, state: { user } }}
+            className="site-title"
+          >
+            Profile {}
+          </Link>
+        </li>
+        <li>
+          {/* You can also pass props in a similar way for other Links */}
+          <Link to="#" className={"link"} onClick={handleLogout}>
+            Logout
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default UserNavbar;
