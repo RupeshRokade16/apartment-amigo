17de7815907ba03fdf04cbc5f80ec3096fe99e6a
aakanksha-arun <desai.aaka@northeastern.edu>
2023-12-07 16:50:25 -0500

    fixed backend url

--- client/src/utils/backendUrlPrefix.js
+++ client/src/utils/backendUrlPrefix.js
@@ -2,6 +2,6 @@ import PORT from "./configure2";
 
 const port = PORT || 5000;
 
-const backendUrlPrefix=`apartment-amigo.azurewebsites.net`;
+const backendUrlPrefix=`https://apartment-amigo.azurewebsites.net`;
 
 export default backendUrlPrefix;
